9b87ea41e230bb7573ba4d0aa4439294112ff9d2
jiangplus <jiang.plus.times@gmail.com>
2021-02-02 11:11:03 +0800

    extract s3 methods

--- main.go
+++ main.go
@@ -4,14 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"net/url"
 	"os"
-	"path/filepath"
 	"strings"
-
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
 func getPath(path string) string {
@@ -43,189 +37,53 @@ func main() {
 			svc, sess := CreateS3Client()
 			ListS3Bucket(sess, svc)
 		} else {
-			svc, _ := CreateS3Client()
 			s3path := lsCmd.Arg(0)
-			s3url, err := url.Parse(s3path)
-			if err != nil {
-				log.Fatal(err)
-			}
-			if s3url.Scheme != "s3" {
-				log.Fatal("path must be s3 url, like: s3://bucket")
-			}
-			bucket := s3url.Host
-			path := strings.TrimPrefix(s3url.Path, "/")
-			obj_result, err := svc.ListObjectsV2(&s3.ListObjectsV2Input{Bucket: aws.String(bucket), Prefix: aws.String(path), Delimiter: aws.String("/")})
-			if err != nil {
-				exitErrorf("Unable to list objects, %v", err)
-			}
-			fmt.Println("Objects:")
-			for _, o := range obj_result.CommonPrefixes {
-				fmt.Printf("s3://%s/%s\n", bucket,
-					aws.StringValue(o.Prefix))
-			}
-
-			for _, o := range obj_result.Contents {
-				fmt.Printf("%s %s %d s3://%s/%s\n", aws.TimeValue(o.LastModified), unmarkEtag(aws.StringValue(o.ETag)), aws.Int64Value(o.Size),
-					bucket, aws.StringValue(o.Key))
-			}
+			ListObjects(s3path)
 		}
 	case "get":
 		getCmd.Parse(os.Args[2:])
 		if len(getCmd.Args()) != 2 {
 			log.Fatal("source and dest is required")
 		} else {
-			_, sess := CreateS3Client()
 			local_file_path := getCmd.Arg(1)
-			s3url, err := url.Parse(getCmd.Arg(0))
-			if err != nil {
-				log.Fatal(err)
-			}
-			if s3url.Scheme != "s3" {
-				log.Fatal("path must be s3 url, like: s3://bucket")
-			}
-			bucket := s3url.Host
-			path := getPath(s3url.Path)
+			s3path := getCmd.Arg(0)
 
-			file, err := os.Create(local_file_path)
-			if err != nil {
-				os.MkdirAll(filepath.Dir(local_file_path), os.ModePerm)
-				file, err = os.Create(local_file_path)
-				if err != nil {
-					exitErrorf("Unable to open file %q, %v", local_file_path, err)
-				}
-			}
-			defer file.Close()
-
-			downloader := s3manager.NewDownloader(sess)
-			numBytes, err := downloader.Download(file,
-				&s3.GetObjectInput{
-					Bucket: aws.String(bucket),
-					Key:    &path,
-				})
-			if err != nil {
-				exitErrorf("Unable to download item %q, %v", local_file_path, err)
-			}
-
-			fmt.Println("Downloaded", file.Name(), numBytes, "bytes")
+			GetObject(s3path, local_file_path)
 		}
 	case "put":
 		putCmd.Parse(os.Args[2:])
 		if len(putCmd.Args()) != 2 {
 			log.Fatal("source and dest is required")
 		} else {
-			_, sess := CreateS3Client()
 			local_file_path := putCmd.Arg(0)
-			s3url, err := url.Parse(putCmd.Arg(1))
-			if err != nil {
-				log.Fatal(err)
-			}
-			if s3url.Scheme != "s3" {
-				log.Fatal("path must be s3 url, like: s3://bucket")
-			}
-			bucket := s3url.Host
-			targetPath := getPath(s3url.Path)
-
-			uploader := s3manager.NewUploader(sess)
-			file, err := os.Open(local_file_path)
-			if err != nil {
-				log.Println("Failed opening file", local_file_path, err)
-			}
-			defer file.Close()
-			result, err := uploader.Upload(&s3manager.UploadInput{
-				Bucket: &bucket,
-				Key:    aws.String(targetPath),
-				Body:   file,
-			})
-			if err != nil {
-				log.Fatalln("Failed to upload", local_file_path, err)
-			}
-			fmt.Printf("Uploaded %s s3://%s/%s at %s", local_file_path, bucket, targetPath, result.Location)
+			s3path := putCmd.Arg(1)
+			PutObject(s3path, local_file_path)
 		}
 	case "rm":
 		rmCmd.Parse(os.Args[2:])
 		if len(rmCmd.Args()) == 0 {
 			log.Fatal("s3 uri is required")
 		} else {
-			svc, _ := CreateS3Client()
-			s3url, err := url.Parse(rmCmd.Arg(0))
-			if err != nil {
-				log.Fatal(err)
-			}
-			if s3url.Scheme != "s3" {
-				log.Fatal("path must be s3 url, like: s3://bucket")
-			}
-			bucket := s3url.Host
-			targetPath := getPath(s3url.Path)
-
-			result, err := svc.DeleteObject(&s3.DeleteObjectInput{Bucket: aws.String(bucket), Key: aws.String(targetPath)})
-			if err != nil {
-				exitErrorf("Unable to list objects, %v, %s, %s", err)
-			}
-			log.Println(result)
+			s3path := rmCmd.Arg(0)
+			RmObject(s3path)
 		}
 	case "info":
 		infoCmd.Parse(os.Args[2:])
 		if len(infoCmd.Args()) == 0 {
 			log.Fatal("s3 uri is required")
 		} else {
-			svc, _ := CreateS3Client()
-			s3url, err := url.Parse(infoCmd.Arg(0))
-			if err != nil {
-				log.Fatal(err)
-			}
-			if s3url.Scheme != "s3" {
-				log.Fatal("path must be s3 url, like: s3://bucket")
-			}
-			bucket := s3url.Host
-			targetPath := getPath(s3url.Path)
-
-			acl_result, err := svc.GetObjectAcl(&s3.GetObjectAclInput{Bucket: aws.String(bucket), Key: aws.String(targetPath)})
-			if err != nil {
-				exitErrorf("Unable to list objects, %v, %s, %s", err)
-			}
-			log.Println(acl_result)
-
-			result, err := svc.HeadObject(&s3.HeadObjectInput{Bucket: aws.String(bucket), Key: aws.String(targetPath)})
-			if err != nil {
-				exitErrorf("Unable to list objects, %v, %s, %s", err)
-			}
-			log.Println(result)
+			s3path := infoCmd.Arg(0)
+			ObjectInfo(s3path)
 		}
 	case "cp":
 		cpCmd.Parse(os.Args[2:])
 		if len(cpCmd.Args()) != 2 {
 			log.Fatal("source and dest is required")
 		} else {
-			svc, _ := CreateS3Client()
-			srcurl, err := url.Parse(cpCmd.Arg(0))
-			if err != nil {
-				log.Fatal(err)
-			}
-			if srcurl.Scheme != "s3" {
-				log.Fatal("path must be s3 url, like: s3://bucket")
-			}
-			srcbucket := srcurl.Host
-			srcpath := getPath(srcurl.Path)
+			src := cpCmd.Arg(0)
+			dst := cpCmd.Arg(1)
 
-			dsturl, err := url.Parse(cpCmd.Arg(0))
-			if err != nil {
-				log.Fatal(err)
-			}
-			if srcurl.Scheme != "s3" {
-				log.Fatal("path must be s3 url, like: s3://bucket")
-			}
-			dstbucket := dsturl.Host
-			dstpath := getPath(dsturl.Path)
-
-			if srcbucket != dstbucket {
-				exitErrorf("source and dest must be in the same bucket")
-			}
-
-			result, err := svc.CopyObject(&s3.CopyObjectInput{Bucket: aws.String(dstbucket), CopySource: aws.String(srcpath), Key: aws.String(dstpath)})
-			if err != nil {
-				exitErrorf("Unable to list objects, %v, %s, %s", err)
-			}
-			log.Println(result)
+			CopyObject(src, dst)
 		}
 	case "modify":
 		modifyCmd.Parse(os.Args[2:])
@@ -237,56 +95,17 @@ func main() {
 		if len(setaclCmd.Args()) == 0 {
 			log.Fatal("s3 uri is required")
 		} else {
-			svc, _ := CreateS3Client()
-			s3url, err := url.Parse(setaclCmd.Arg(0))
-			if err != nil {
-				log.Fatal(err)
-			}
-			if s3url.Scheme != "s3" {
-				log.Fatal("path must be s3 url, like: s3://bucket")
-			}
-			bucket := s3url.Host
-			targetPath := getPath(s3url.Path)
-
-			var permission string
-			if *set_acl_public {
-				permission = "public-read"
-			} else if *set_acl_private {
-				permission = "private"
-			} else {
-				permission = "private"
-			}
-
-			_, err = svc.PutObjectAcl(&s3.PutObjectAclInput{Bucket: aws.String(bucket), Key: aws.String(targetPath), ACL: aws.String(permission)})
-			if err != nil {
-				exitErrorf("Unable to set object acl, %v, %s, %s", err)
-			}
-			fmt.Println("ok")
+			s3path := setaclCmd.Arg(0)
+			SetaclObject(s3path, set_acl_public, set_acl_private)
 		}
 	case "sync":
 		syncCmd.Parse(os.Args[2:])
 		if len(getCmd.Args()) != 2 {
 			log.Fatal("source and dest is required")
 		} else {
-			srcurl, err := url.Parse(setaclCmd.Arg(0))
-			if err != nil {
-				log.Fatal(err)
-			}
-			dsturl, err := url.Parse(setaclCmd.Arg(1))
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			svc, sess := CreateS3Client()
-			if srcurl.Scheme == "s3" && dsturl.Scheme == "s3" {
-				log.Fatal("both source and dest are s3 address is not supported")
-			} else if srcurl.Scheme == "s3" {
-				DownloadS3Dir(sess, svc, setaclCmd.Arg(0), setaclCmd.Arg(1))
-			} else if dsturl.Scheme == "s3" {
-				UploadS3Dir(sess, svc, setaclCmd.Arg(0), setaclCmd.Arg(1))
-			} else {
-				log.Fatal("not supported")
-			}
+			srcpath := setaclCmd.Arg(0)
+			dstpath := setaclCmd.Arg(1)
+			SyncDir(srcpath, dstpath)
 
 		}
 	case "multipart":
@@ -294,77 +113,26 @@ func main() {
 		if len(multipartCmd.Args()) == 0 {
 			log.Fatal("s3 uri is required")
 		} else {
-			svc, _ := CreateS3Client()
-			s3url, err := url.Parse(multipartCmd.Arg(0))
-			if err != nil {
-				log.Fatal(err)
-			}
-			if s3url.Scheme != "s3" {
-				log.Fatal("path must be s3 url, like: s3://bucket")
-			}
-			bucket := s3url.Host
-
-			result, err := svc.ListMultipartUploads(&s3.ListMultipartUploadsInput{Bucket: aws.String(bucket)})
-			if err != nil {
-				exitErrorf("Unable to list objects, %v, %s, %s", err)
-			}
-			log.Println(result)
-
-			fmt.Println("Initiated\tPath\tId")
-			for _, item := range result.Uploads {
-				fmt.Println(item.Initiated, aws.StringValue(item.Key), aws.StringValue(item.UploadId))
-			}
+			s3path := multipartCmd.Arg(0)
+			ListMultiParts(s3path)
 		}
 	case "abortmp":
 		abortmpCmd.Parse(os.Args[2:])
 		if len(abortmpCmd.Args()) == 0 {
 			log.Fatal("s3 uri is required")
 		} else {
-			svc, _ := CreateS3Client()
-			s3url, err := url.Parse(abortmpCmd.Arg(0))
-			if err != nil {
-				log.Fatal(err)
-			}
-			if s3url.Scheme != "s3" {
-				log.Fatal("path must be s3 url, like: s3://bucket")
-			}
-			bucket := s3url.Host
-			path := getPath(s3url.Path)
+			s3path := abortmpCmd.Arg(0)
 			upload_id := abortmpCmd.Arg(1)
-
-			result, err := svc.AbortMultipartUpload(&s3.AbortMultipartUploadInput{Bucket: aws.String(bucket), Key: aws.String(path), UploadId: aws.String(upload_id)})
-			if err != nil {
-				exitErrorf("Unable to list objects, %v, %s, %s", err)
-			}
-			log.Println(result)
+			AbortMultiPart(s3path, upload_id)
 		}
 	case "listmp":
 		listmpCmd.Parse(os.Args[2:])
 		if len(listmpCmd.Args()) == 0 {
 			log.Fatal("s3 uri is required")
 		} else {
-			svc, _ := CreateS3Client()
-			s3url, err := url.Parse(listmpCmd.Arg(0))
-			if err != nil {
-				log.Fatal(err)
-			}
-			if s3url.Scheme != "s3" {
-				log.Fatal("path must be s3 url, like: s3://bucket")
-			}
-			bucket := s3url.Host
-			path := getPath(s3url.Path)
+			s3path := listmpCmd.Arg(0)
 			upload_id := listmpCmd.Arg(1)
-
-			result, err := svc.ListParts(&s3.ListPartsInput{Bucket: aws.String(bucket), Key: aws.String(path), UploadId: aws.String(upload_id)})
-			if err != nil {
-				exitErrorf("Unable to list objects, %v, %s, %s", err)
-			}
-			log.Println(result)
-
-			fmt.Println("LastModified\t\t\tPartNumber\tETag\tSize")
-			for _, item := range result.Parts {
-				fmt.Println(item.LastModified, aws.Int64Value(item.PartNumber), aws.StringValue(item.ETag), aws.Int64Value(item.Size))
-			}
+			MultiPartDetail(s3path, upload_id)
 		}
 	default:
 		fmt.Println("expected subcommands to run")
--- s3.go
+++ s3.go
@@ -56,19 +56,15 @@ func ListS3Bucket(sess *session.Session, svc *s3.S3) {
 	}
 }
 
-func UploadS3Dir(sess *session.Session, svc *s3.S3, src string, dst string) {
-	dstUrl, err := url.Parse(dst)
-	if err != nil {
-		panic(err)
-	}
-	bucket := dstUrl.Host
-	targetPath := strings.TrimPrefix(dstUrl.Path, "/")
+func UploadS3Dir(sess *session.Session, svc *s3.S3, src *url.URL, dst *url.URL) {
+	bucket := dst.Host
+	targetPath := strings.TrimPrefix(dst.Path, "/")
 	fmt.Println("o", src, dst, bucket, targetPath)
 
 	walker := make(fileWalk)
 	go func() {
 		// Gather the files to upload by walking the path recursively
-		if err := filepath.Walk(src, walker.Walk); err != nil {
+		if err := filepath.Walk(src.Path, walker.Walk); err != nil {
 			log.Fatalln("Walk failed:", err)
 		}
 		close(walker)
@@ -77,7 +73,7 @@ func UploadS3Dir(sess *session.Session, svc *s3.S3, src string, dst string) {
 	// For each file found walking, upload it to S3
 	uploader := s3manager.NewUploader(sess)
 	for path := range walker {
-		rel, err := filepath.Rel(src, path)
+		rel, err := filepath.Rel(src.Path, path)
 		if err != nil {
 			log.Fatalln("Unable to get relative path:", path, err)
 		}
@@ -99,14 +95,9 @@ func UploadS3Dir(sess *session.Session, svc *s3.S3, src string, dst string) {
 	}
 }
 
-func DownloadS3Dir(sess *session.Session, svc *s3.S3, src string, dst string) {
-	srcUrl, err := url.Parse(src)
-	if err != nil {
-		log.Fatal(err)
-		panic(err)
-	}
-	bucket := srcUrl.Host
-	path := strings.TrimPrefix(srcUrl.Path, "/")
+func DownloadS3Dir(sess *session.Session, svc *s3.S3, src *url.URL, dst *url.URL) {
+	bucket := src.Host
+	path := strings.TrimPrefix(src.Path, "/")
 	obj_result, err := svc.ListObjectsV2(&s3.ListObjectsV2Input{Bucket: aws.String(bucket), Prefix: aws.String(path), Delimiter: aws.String("/")})
 	if err != nil {
 		exitErrorf("Unable to list objects, %v, %s, %s", err, src, dst)
@@ -120,7 +111,7 @@ func DownloadS3Dir(sess *session.Session, svc *s3.S3, src string, dst string) {
 
 		item := aws.StringValue(o.Key)
 		item = strings.TrimPrefix(item, path)
-		item = filepath.Join(dst, item)
+		item = filepath.Join(dst.Path, item)
 		file, err := os.Create(item)
 		if err != nil {
 			os.MkdirAll(filepath.Dir(item), os.ModePerm)
@@ -141,5 +132,289 @@ func DownloadS3Dir(sess *session.Session, svc *s3.S3, src string, dst string) {
 		}
 
 		fmt.Println("Downloaded", file.Name(), numBytes, "bytes")
+	}
+}
+
+func RmObject(s3path string) {
+	svc, _ := CreateS3Client()
+	s3url, err := url.Parse(s3path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if s3url.Scheme != "s3" {
+		log.Fatal("path must be s3 url, like: s3://bucket")
+	}
+	bucket := s3url.Host
+	targetPath := getPath(s3url.Path)
+
+	result, err := svc.DeleteObject(&s3.DeleteObjectInput{Bucket: aws.String(bucket), Key: aws.String(targetPath)})
+	if err != nil {
+		exitErrorf("Unable to list objects, %v, %s, %s", err)
+	}
+	log.Println(result)
+}
+
+func ObjectInfo(s3path string) {
+	svc, _ := CreateS3Client()
+	s3url, err := url.Parse(s3path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if s3url.Scheme != "s3" {
+		log.Fatal("path must be s3 url, like: s3://bucket")
+	}
+	bucket := s3url.Host
+	targetPath := getPath(s3url.Path)
+
+	acl_result, err := svc.GetObjectAcl(&s3.GetObjectAclInput{Bucket: aws.String(bucket), Key: aws.String(targetPath)})
+	if err != nil {
+		exitErrorf("Unable to list objects, %v, %s, %s", err)
+	}
+	log.Println(acl_result)
+
+	result, err := svc.HeadObject(&s3.HeadObjectInput{Bucket: aws.String(bucket), Key: aws.String(targetPath)})
+	if err != nil {
+		exitErrorf("Unable to list objects, %v, %s, %s", err)
+	}
+	log.Println(result)
+}
+
+func ListObjects(s3path string) {
+	svc, _ := CreateS3Client()
+	s3url, err := url.Parse(s3path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if s3url.Scheme != "s3" {
+		log.Fatal("path must be s3 url, like: s3://bucket")
+	}
+	bucket := s3url.Host
+	path := strings.TrimPrefix(s3url.Path, "/")
+	obj_result, err := svc.ListObjectsV2(&s3.ListObjectsV2Input{Bucket: aws.String(bucket), Prefix: aws.String(path), Delimiter: aws.String("/")})
+	if err != nil {
+		exitErrorf("Unable to list objects, %v", err)
+	}
+	fmt.Println("Objects:")
+	for _, o := range obj_result.CommonPrefixes {
+		fmt.Printf("s3://%s/%s\n", bucket,
+			aws.StringValue(o.Prefix))
+	}
+
+	for _, o := range obj_result.Contents {
+		fmt.Printf("%s %s %d s3://%s/%s\n", aws.TimeValue(o.LastModified), unmarkEtag(aws.StringValue(o.ETag)), aws.Int64Value(o.Size),
+			bucket, aws.StringValue(o.Key))
+	}
+}
+
+func GetObject(s3path string, local_file_path string) {
+	_, sess := CreateS3Client()
+	s3url, err := url.Parse(s3path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if s3url.Scheme != "s3" {
+		log.Fatal("path must be s3 url, like: s3://bucket")
+	}
+	bucket := s3url.Host
+	path := getPath(s3url.Path)
+
+	file, err := os.Create(local_file_path)
+	if err != nil {
+		os.MkdirAll(filepath.Dir(local_file_path), os.ModePerm)
+		file, err = os.Create(local_file_path)
+		if err != nil {
+			exitErrorf("Unable to open file %q, %v", local_file_path, err)
+		}
+	}
+	defer file.Close()
+
+	downloader := s3manager.NewDownloader(sess)
+	numBytes, err := downloader.Download(file,
+		&s3.GetObjectInput{
+			Bucket: aws.String(bucket),
+			Key:    &path,
+		})
+	if err != nil {
+		exitErrorf("Unable to download item %q, %v", local_file_path, err)
+	}
+
+	fmt.Println("Downloaded", file.Name(), numBytes, "bytes")
+}
+
+func PutObject(s3path string, local_file_path string) {
+	_, sess := CreateS3Client()
+	s3url, err := url.Parse(s3path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if s3url.Scheme != "s3" {
+		log.Fatal("path must be s3 url, like: s3://bucket")
+	}
+	bucket := s3url.Host
+	targetPath := getPath(s3url.Path)
+
+	uploader := s3manager.NewUploader(sess)
+	file, err := os.Open(local_file_path)
+	if err != nil {
+		log.Println("Failed opening file", local_file_path, err)
+	}
+	defer file.Close()
+	result, err := uploader.Upload(&s3manager.UploadInput{
+		Bucket: &bucket,
+		Key:    aws.String(targetPath),
+		Body:   file,
+	})
+	if err != nil {
+		log.Fatalln("Failed to upload", local_file_path, err)
+	}
+	fmt.Printf("Uploaded %s s3://%s/%s at %s", local_file_path, bucket, targetPath, result.Location)
+}
+
+func CopyObject(src string, dst string) {
+	svc, _ := CreateS3Client()
+	srcurl, err := url.Parse(src)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if srcurl.Scheme != "s3" {
+		log.Fatal("path must be s3 url, like: s3://bucket")
+	}
+	srcbucket := srcurl.Host
+	srcpath := getPath(srcurl.Path)
+
+	dsturl, err := url.Parse(dst)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if srcurl.Scheme != "s3" {
+		log.Fatal("path must be s3 url, like: s3://bucket")
+	}
+	dstbucket := dsturl.Host
+	dstpath := getPath(dsturl.Path)
+
+	if srcbucket != dstbucket {
+		exitErrorf("source and dest must be in the same bucket")
+	}
+
+	result, err := svc.CopyObject(&s3.CopyObjectInput{Bucket: aws.String(dstbucket), CopySource: aws.String(srcpath), Key: aws.String(dstpath)})
+	if err != nil {
+		exitErrorf("Unable to list objects, %v, %s, %s", err)
+	}
+	log.Println(result)
+}
+
+func SetaclObject(s3path string, set_acl_public *bool, set_acl_private *bool) {
+	svc, _ := CreateS3Client()
+	s3url, err := url.Parse(s3path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if s3url.Scheme != "s3" {
+		log.Fatal("path must be s3 url, like: s3://bucket")
+	}
+	bucket := s3url.Host
+	targetPath := getPath(s3url.Path)
+
+	var permission string
+	if *set_acl_public {
+		permission = "public-read"
+	} else if *set_acl_private {
+		permission = "private"
+	} else {
+		permission = "private"
+	}
+
+	_, err = svc.PutObjectAcl(&s3.PutObjectAclInput{Bucket: aws.String(bucket), Key: aws.String(targetPath), ACL: aws.String(permission)})
+	if err != nil {
+		exitErrorf("Unable to set object acl, %v, %s, %s", err)
+	}
+	fmt.Println("ok")
+}
+
+func SyncDir(srcpath string, dstpath string) {
+	srcurl, err := url.Parse(srcpath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	dsturl, err := url.Parse(dstpath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	svc, sess := CreateS3Client()
+	if srcurl.Scheme == "s3" && dsturl.Scheme == "s3" {
+		log.Fatal("both source and dest are s3 address is not supported")
+	} else if srcurl.Scheme == "s3" {
+		DownloadS3Dir(sess, svc, srcurl, dsturl)
+	} else if dsturl.Scheme == "s3" {
+		UploadS3Dir(sess, svc, srcurl, dsturl)
+	} else {
+		log.Fatal("not supported")
+	}
+}
+
+func ListMultiParts(s3path string) {
+	svc, _ := CreateS3Client()
+	s3url, err := url.Parse(s3path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if s3url.Scheme != "s3" {
+		log.Fatal("path must be s3 url, like: s3://bucket")
+	}
+	bucket := s3url.Host
+
+	result, err := svc.ListMultipartUploads(&s3.ListMultipartUploadsInput{Bucket: aws.String(bucket)})
+	if err != nil {
+		exitErrorf("Unable to list objects, %v, %s, %s", err)
+	}
+	log.Println(result)
+
+	fmt.Println("Initiated\tPath\tId")
+	for _, item := range result.Uploads {
+		fmt.Println(item.Initiated, aws.StringValue(item.Key), aws.StringValue(item.UploadId))
+	}
+}
+
+func AbortMultiPart(s3path string, upload_id string) {
+	s3url, err := url.Parse(s3path)
+	svc, _ := CreateS3Client()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if s3url.Scheme != "s3" {
+		log.Fatal("path must be s3 url, like: s3://bucket")
+	}
+	bucket := s3url.Host
+	path := getPath(s3url.Path)
+
+	result, err := svc.AbortMultipartUpload(&s3.AbortMultipartUploadInput{Bucket: aws.String(bucket), Key: aws.String(path), UploadId: aws.String(upload_id)})
+	if err != nil {
+		exitErrorf("Unable to list objects, %v, %s, %s", err)
+	}
+	log.Println(result)
+}
+
+func MultiPartDetail(s3path string, upload_id string) {
+	svc, _ := CreateS3Client()
+	s3url, err := url.Parse(s3path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if s3url.Scheme != "s3" {
+		log.Fatal("path must be s3 url, like: s3://bucket")
+	}
+	bucket := s3url.Host
+	path := getPath(s3url.Path)
+
+	result, err := svc.ListParts(&s3.ListPartsInput{Bucket: aws.String(bucket), Key: aws.String(path), UploadId: aws.String(upload_id)})
+	if err != nil {
+		exitErrorf("Unable to list objects, %v, %s, %s", err)
+	}
+	log.Println(result)
+
+	fmt.Println("LastModified\t\t\tPartNumber\tETag\tSize")
+	for _, item := range result.Parts {
+		fmt.Println(item.LastModified, aws.Int64Value(item.PartNumber), aws.StringValue(item.ETag), aws.Int64Value(item.Size))
 	}
 }
